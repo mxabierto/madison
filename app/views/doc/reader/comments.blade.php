fe3fdae25358cef4f1c45347dbf57199bdbb199d
Omar <cone-7@hotmail.com>
2015-01-15 17:46:32 -0600

    Correccion para mostrar los comentarios que solo son visibles

--- app/views/doc/reader/comments.blade.php
+++ app/views/doc/reader/comments.blade.php
@@ -5,7 +5,6 @@
   @if(Auth::user()->hasRole('Admin'))
     <div ng-init="admin=true"></div>
   @endif
-
   @else
   <div id="participate-comment" class="participate-comment">
   	<p>{{ trans('messages.please') }} <a href="{{ url('/user/login', $parameters = array(), $secure = null) }}" target="_self">{{ trans('messages.login') }}</a> {{ trans('messages.tocomment') }}.</p>
