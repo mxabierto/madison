1f346ef734ca74da4cd55842da5b3c0f63b692f7
Rod Wilhelmy <rwilhelmy@gmail.com>
2015-03-18 11:24:42 -0600

    Fix small typo in comment-box placeholder

--- app/views/doc/reader/votes/comment.blade.php
+++ app/views/doc/reader/votes/comment.blade.php
@@ -1,8 +1,8 @@
 <div class="comment-field" ng-show="user.id">
   <form name="add-comment-form" ng-submit="commentSubmit()">
     <div class="form-group">
-      <label for="doc-comment-field">Sugiere otro categoría:</label>
-      <input ng-model="comment.text" id="doc-comment-field" type="text" class="form-control centered" placeholder="Sugiere otro categoría" required />
+      <label for="doc-comment-field">Sugiere otra categoría:</label>
+      <input ng-model="comment.text" id="doc-comment-field" type="text" class="form-control centered" placeholder="Sugiere otra categoría" required />
     </div>
     <hr>
   </form>
