40b7dde3467302bcae8789fd2601332361925894
Carlos Cessa <carlos@pixative.com>
2015-06-01 17:02:47 -0500

    Services Refactor
    - Refactor application services code to seperate the available
    services in individual files
    - Modified grunt task to include the added files when building the
    application

---
+++ public/js/services/AnnotationService.js
@@ -0,0 +1,32 @@
+angular.module( 'madisonApp.services' )
+    .factory( 'annotationService', [ '$rootScope', '$sce', function ( $rootScope, $sce ) {
+        var annotationService           = {};
+        var converter                   = new Markdown.Converter();
+        annotationService.annotations   = [];
+
+        annotationService.setAnnotations    = function ( annotations ) {
+            angular.forEach(annotations, function ( annotation ) {
+                annotation.html = $sce.trustAsHtml( converter.makeHtml( annotation.text ) );
+                this.annotations.push( annotation );
+            }, this );
+
+            this.broadcastUpdate();
+        };
+        annotationService.addAnnotation     = function ( annotation ) {
+            if ( annotation.id === undefined ) {
+                var interval    = window.setInterval( function () {
+                    this.addAnnotation( annotation );
+                    window.clearInterval( interval );
+                }.bind(this), 500 );
+            } else {
+                annotation.html = $sce.trustAsHtml(converter.makeHtml(annotation.text));
+                this.annotations.push(annotation);
+                this.broadcastUpdate();
+            }
+        };
+        annotationService.broadcastUpdate   = function () {
+            $rootScope.$broadcast( 'annotationsUpdated' );
+        };
+
+        return annotationService;
+    }]);
